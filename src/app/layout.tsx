b79ac07ad68f5781767b32ada72d32539348e364
Mayke F. Santos <sfmayke@gmail.com>
2023-05-22 21:35:45 -0300

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Bellefair, Barlow_Condensed, Barlow } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Bellefair({ subsets: ['latin'], weight: "400" })
+const barlow_condensed = Barlow_Condensed({ subsets: ['latin'], weight: "400", variable: "--font-barlow_condensed" })
+const barlow = Barlow({ subsets: ['latin'], weight: "400", variable: "--font-barlow" })
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${barlow.variable} ${barlow_condensed.variable}`}>
       <body className={inter.className}>{children}</body>
     </html>
   )
