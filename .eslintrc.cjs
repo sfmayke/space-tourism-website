131dbedee7a02aafad8147121bcd85bb0c585d5f
Mayke F. Santos <sfmayke@gmail.com>
2023-05-23 22:43:20 -0300

    🎨 config: project linters config

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+/** @type {import('eslint').Linter.Config} */
+
+module.exports = {
+  extends: [
+    "next/core-web-vitals",
+    "airbnb",
+    "airbnb-typescript",
+    "prettier",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react/jsx-runtime",
+  ],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "variable",
+        format: ["camelCase", "UPPER_CASE", "snake_case"],
+      },
+    ],
+  },
+};
