0346076da3ac1d579ef5c3012f46fc0deb124f4c
Mayke F. Santos <sfmayke@gmail.com>
2023-05-24 21:52:32 -0300

    🔧 tailwind: theme updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,28 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        h1: ["9.375rem", { lineHeight: "10.744rem" }],
+        h2: ["6.25rem", { lineHeight: "7.163rem" }],
+        h3: ["3.5rem", { lineHeight: "4.011rem" }],
+        h4: ["4.011rem", { lineHeight: "2.292rem" }],
+        h5: ["1.75rem", { lineHeight: "2.1rem", letterSpacing: "0.295rem" }],
+        sh1: ["1.75rem", { lineHeight: "2.006rem" }],
+        sh2: ["0.875rem", { lineHeight: "1.05rem", letterSpacing: "0.148rem" }],
+        nav: ["1rem", { lineHeight: "1.2rem", letterSpacing: "0.169rem" }],
+        body: ["1.125rem", { lineHeight: "2rem" }],
+      },
       fontFamily: {
         barlow: ["var(--font-barlow)"],
         "barlow-condensed": ["var(--font-barlow-condensed)"],
+      },
+      colors: {
+        primary: "#0B0D17",
+        secondary: "#D0D6F9",
+      },
+      screens: {
+        phone: "var(--phone-screen)",
+        tablet: "var(--tablet-screen)",
       },
     },
   },
