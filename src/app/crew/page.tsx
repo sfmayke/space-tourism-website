445822328801460c835da0b8db4bc00c253a0516
Mayke F. Santos <sfmayke@gmail.com>
2023-06-02 17:27:38 -0300

    ✨ (destination - page): new page added to site

--- src/app/crew/page.tsx
+++ src/app/crew/page.tsx
@@ -13,21 +13,9 @@ export default function Crew() {
       />
       <PageAnimation>
         <article className="flex flex-col items-center gap-4 tablet:mt-[6.625rem] tablet:max-w-md tablet:gap-6 desktop:items-start">
-          <p className="font-barlow-condensed text-nav uppercase text-secondary tablet:text-xl desktop:text-sh1">
-            so, you want to travel to
-          </p>
-          <h1 className="text-[80px] leading-[100px] tablet:text-h1 tablet:leading-[150px] desktop:text-h1">
-            SPACE
-          </h1>
-          <p
-            className="text-center font-barlow text-body text-secondary 
-          tablet:text-base tablet:leading-6 
-          desktop:text-left desktop:text-[1.125rem] desktop:leading-8"
-          >
-            Let’s face it; if you want to go to space, you might as well
-            genuinely go to outer space and not hover kind of on the edge of it.
-            Well sit back, and relax because we’ll give you a truly out of this
-            world experience!
+          <p className="font-barlow-condensed text-nav uppercase">
+            <span className="mr-4 font-bold opacity-25">01</span>
+            pick your destination
           </p>
         </article>
       </PageAnimation>
--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -1,3 +1,27 @@
+import BackgroundImage from "@/components/ui/background-image";
+import backgroundImage from "@public/images/destination/background-destination-desktop.jpg";
+import Image from "next/image";
+import moonImage from "@public/images/destination/image-moon.png";
+import PageAnimation from "../pageAnimation";
+
 export default function Destination() {
-  return <h1>hello</h1>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-6 tablet:pt-0 desktop:flex-row desktop:p-40">
+      <BackgroundImage
+        srcDefault={backgroundImage}
+        srcTablet="/images/destination/background-destination-tablet.jpg"
+        srcMobile="/images/destination/background-destination-mobile.jpg"
+        alt="destination-background"
+      />
+      <PageAnimation>
+        <article className="flex flex-col items-center gap-4 tablet:mt-[6.625rem] tablet:max-w-md tablet:gap-6 desktop:items-start">
+          <p className="font-barlow-condensed text-nav uppercase">
+            <span className="mr-4 font-bold opacity-25">01</span>
+            pick your destination
+          </p>
+          <Image src={moonImage} alt="moon-planet" className="mt-8 h-44 w-44" />
+        </article>
+      </PageAnimation>
+    </div>
+  );
 }
