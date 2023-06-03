58ec1abc7aba7d8b5642ee191efe506a0c351086
Mayke F. Santos <sfmayke@gmail.com>
2023-06-03 10:58:15 -0300

    💄 (divider): component added

---
+++ src/components/ui/divider.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import * as Separator from "@radix-ui/react-separator";
+
+export default function Divider() {
+  return <Separator.Root className="my-8 h-[1px] bg-[#383B4B]" />;
+}
